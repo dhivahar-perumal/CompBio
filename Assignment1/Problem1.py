ac0d01a2a89f79467759ec042ac7642e7c25d66e
Dhivahar <31513925+tamil-paiyan@users.noreply.github.com>
2018-09-04 17:36:30 -0400

    Problem1 completed

--- Assignment1/Problem1.py
+++ Assignment1/Problem1.py
@@ -1,5 +1,5 @@
 def main():
-    path = 'C:/Users/Dhiva/Desktop/problem1.txt'
+    path = 'C:/Users/Dhiva/Desktop/rosalind_dna.txt'
     protein = {
         "A" : 0,
         "C": 0,
@@ -10,7 +10,8 @@ def main():
     str = inputFile.read()
     print (str)
     for c in str:
-        protein[c] = protein[c] + 1
+        if c in protein:
+            protein[c] = protein[c] + 1
     print (protein["A"],protein["C"],protein["G"],protein["T"])
 
 if __name__ == "__main__":

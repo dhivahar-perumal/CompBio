25b0027f4dc0e26cd56fe585de5f6b081f42c4e9
Dhivahar <31513925+tamil-paiyan@users.noreply.github.com>
2018-09-30 11:53:50 -0400

    Problem11 completed

--- Assignment2/Problem11.py
+++ Assignment2/Problem11.py
@@ -1,41 +1,38 @@
 def main():
-    inputFile = open('C:/Users/Dhiva/Desktop/input.txt', 'r')
+    inputFile = open('C:/Users/Dhiva/Desktop/rosalind_ba9a.txt', 'r')
+    inputFile = open('C:/Users/Dhiva/Desktop/rosalind_ba9a.txt', 'r')
     outputFile = open('C:/Users/Dhiva/Desktop/output.txt', 'w+')
     string = inputFile.read()
     patterns = string.split()
-    trieConstruction(patterns)
-    print(patterns)
+    nodes = trieConstruction(patterns)
+    for node in nodes:
+        for i in nodes[node]:
+            outputFile.write(str(node)+"->"+str(i[0])+":"+i[1]+"\n")
+    print (nodes)
 
 def trieConstruction(patterns):
-    trie = []
     nodes = dict()
     nodeNum = 0
     for pattern in patterns:
         currentNode = 0
         for i in pattern:
-            print (str(currentNode) + " " + i)
             if currentNode in nodes:
-                #for j in nodes[currentNode]:
-                #    print (j)
-                if nodes[currentNode][0][1] == i:
-                    currentNode = nodes[currentNode][0][0]
-                    print ("currentNode = " + str(currentNode))
+                for j in nodes[currentNode]:
+                    if j[1] == i:
+                        break
+                if j[1] == i:
+                    currentNode = j[0]
                 else:
                     nodeNum += 1
                     tempList = nodes[currentNode]
                     tempList.append((nodeNum, i))
                     nodes[currentNode] = tempList
-                    print (tempList)
-                    print (nodes[currentNode])
                     currentNode = nodeNum
-                    print(nodes)
             else:
                 nodeNum += 1
                 nodes[currentNode] = [(nodeNum, i)]
                 currentNode = nodeNum
-                print (nodes)
-    print (nodes)
-
+    return nodes
 
 
 if __name__ == "__main__":

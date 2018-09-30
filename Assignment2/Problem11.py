c7be1de3cb2a3384b68b3e6121947b27a374dd73
Dhivahar <31513925+tamil-paiyan@users.noreply.github.com>
2018-09-30 10:14:35 -0400

    Problem11 in progress

--- Assignment2/Problem11.py
+++ Assignment2/Problem11.py
@@ -15,18 +15,23 @@ def trieConstruction(patterns):
         for i in pattern:
             print (str(currentNode) + " " + i)
             if currentNode in nodes:
-                if nodes[currentNode][1] == i:
-                    currentNode = nodes[currentNode][0]
+                #for j in nodes[currentNode]:
+                #    print (j)
+                if nodes[currentNode][0][1] == i:
+                    currentNode = nodes[currentNode][0][0]
                     print ("currentNode = " + str(currentNode))
                 else:
                     nodeNum += 1
-                    #tempList = [nodeNum, i]
-                    nodes[currentNode] = [nodeNum, i]
+                    tempList = nodes[currentNode]
+                    tempList.append((nodeNum, i))
+                    nodes[currentNode] = tempList
+                    print (tempList)
+                    print (nodes[currentNode])
                     currentNode = nodeNum
                     print(nodes)
             else:
                 nodeNum += 1
-                nodes[currentNode] = [nodeNum,i]
+                nodes[currentNode] = [(nodeNum, i)]
                 currentNode = nodeNum
                 print (nodes)
     print (nodes)
